7410da6a820b8eae1eead28519f6ddcb1c342b9d
ueveton <uevetonsp89@gmail.com>
2023-01-11 12:18:57 -0300

    add envent split

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -19,13 +19,22 @@ class CalcContoller {
         }, 1000);
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event =>{
+            element.addEventListener(event, fn, false);
+        });
+    }
+
     initializeButtons(){
 
         let buttons = document.querySelectorAll('#buttons > g, #parts >g');
 
         buttons.forEach((btn, index)=> {
-            btn.addEventListener('click', e =>{
+            this.addEventListenerAll(btn, 'click drag', e =>{
                 console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e=>{
+                btn.style.cursor = 'pointer';
             });
         });
     }
